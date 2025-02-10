62cbad5729053ef783dddf985f319bfe425df046
maario <ammaryasser12603@gmail.com>
2025-02-10 21:26:55 +0200

    Update main.py to process red color and adjust HSV limits calculation in utils.py

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 import cv2
+from PIL import Image
 from utils import get_limits
 
 
-yellow = [0, 255, 255]
+red = [255, 0, 0]
 #Just for testing the camera
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 while True:
     ret, frame = cap.read()
 
     hsvImage= cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    lower_limit, upper_limit = get_limits(color=yellow)
+    lower_limit, upper_limit = get_limits(color=red)
 
-    mask = cv2.inRange(hsvImage, lower_limit, upper_limit, hsvImage)
+    mask = cv2.inRange(hsvImage, lower_limit, upper_limit)
+
+    mask_ = Image.fromarray(mask)
+
+    # Convert the image back to numpy array for further processing
+    bbox= mask_.getbbox() # Get the bounding box of the image
+
+    if bbox is not None:
+      x1, y1,x2,y2 = bbox
+      frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 5)  
+
+
     cv2.imshow('Test frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
--- utils.py
+++ utils.py
@@ -7,8 +7,9 @@ def get_limits(color):
     c = np.uint8([[color]])
     hsv_color = cv2.cvtColor(c, cv2.COLOR_BGR2HSV)
 
-    lower_limit = hsv_color[0][0][0] - 10, 100, 100
-    upper_limit = hsv_color[0][0][0] + 10, 255, 255
+    hue = hsv_color[0][0][0]
+    lower_limit = ((hue - 10) % 180, 100, 100)
+    upper_limit = ((hue + 10) % 180, 255, 255)
 
     lower_limit = np.array(lower_limit,dtype=np.uint8)
     upper_limit = np.array(upper_limit,dtype=np.uint8)
