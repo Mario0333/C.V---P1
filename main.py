ce072032b0e3d1c61eed22536b2714378485617d
maario <ammaryasser12603@gmail.com>
2025-02-10 18:56:49 +0200

    Integrate HSV color limit functionality for yellow color in main.py

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import cv2
+from utils import get_limits
 
+
+yellow = [0, 255, 255]
 #Just for testing the camera
 cap = cv2.VideoCapture(0)
 while True:
     ret, frame = cap.read()
-    cv2.imshow('frame', frame)
+
+    hsvImage= cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    lower_limit, upper_limit = get_limits(color=yellow)
+
+    mask = cv2.inRange(hsvImage, lower_limit, upper_limit, hsvImage)
+    cv2.imshow('Test frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 cap.release()
